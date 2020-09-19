8e758df899e1b59c1dde837228aaef7e30f5fa25
hungryc0d3r <shadowhunt2306@gmail.com>
2020-09-19 22:26:59 +0530

    pro1

---
+++ w3Resource/prog1.c
@@ -0,0 +1,9 @@
+/* program 1 :  Write a C program to print your name,
+ date of birth. and mobile number.*/
+#include<stdio.h>
+int main()
+{
+  printf("Name: lucifer\n");
+  printf("DOB: july 14,1999\n");
+  printf("NUM: +19 9999999999");
+}
