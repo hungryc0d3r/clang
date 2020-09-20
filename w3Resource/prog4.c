b862a4b49b2173e6b6436bb6d7a107cd7d1ad724
hungryc0d3r <shadowhunt2306@gmail.com>
2020-09-20 13:21:45 +0530

    prog4

---
+++ w3Resource/prog4.c
@@ -0,0 +1,10 @@
+/*Write a C program to compute the perimeter and area of a rectangle
+ with a height of 7 inches. and width of 5 inches. */
+ #include<stdio.h>
+ int main(){
+    int l=7, b=5;
+    int p=2*(l+b);
+    printf("Perimeter of rectangle= %d inches\n",p);
+    int area=l*b;
+    printf("Area of rectangle= %d square inches\n",area);
+ }
