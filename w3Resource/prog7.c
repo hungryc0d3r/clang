b8acd83afc0cc640c82ae79fa0ef50d5064b3e5f
hungryc0d3r <shadowhunt2306@gmail.com>
2020-09-20 15:27:07 +0530

    prog7

---
+++ w3Resource/prog7.c
@@ -0,0 +1,13 @@
+//Write a C program to convert specified days into years, weeks and days.
+// @hungryc0d3r
+#include<stdio.h>
+int main()
+{
+  int numOfDays=1329;
+  int years=(numOfDays/365);
+  int weeks=(numOfDays%365)/7;
+  int days=numOfDays-(years*365)-(weeks*7);
+    printf("Years: %d \n", years);
+    printf("Weeks: %d \n", weeks);
+    printf("Days: %d \n", days);
+}
