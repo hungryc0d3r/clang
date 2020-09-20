e41a02521d2c5113eec25699ef9c12d817bac705
hungryc0d3r <shadowhunt2306@gmail.com>
2020-09-20 15:37:02 +0530

    prog8, 9

---
+++ w3Resource/prog8.c
@@ -0,0 +1,13 @@
+/*Write a C program that accepts two integers from the user
+and calculate the sum of the two integers.*/
+//@hungryc0d3r
+#include<stdio.h>
+void main(){
+  int a,b;
+  printf("Enter the first number: \n");
+   scanf("%d",&a);
+  printf("Enter the second number: \n");
+   scanf("%d",&b);
+  int c=a+b;
+  printf("Sum of two numbers is : %d",c);
+}
