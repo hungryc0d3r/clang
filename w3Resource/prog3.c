3a3622801582035ba32e3433720151372b75b32a
hungryc0d3r <shadowhunt2306@gmail.com>
2020-09-20 13:12:52 +0530

    prog3

---
+++ w3Resource/prog3.c
@@ -0,0 +1,11 @@
+//Write a C program to print the following characters in a reverse way.
+#include<stdio.h>
+void main(){
+  char c1='X';
+  char c2='M';
+  char c3='L';
+  printf("the characters are :\n");
+  printf("%c \t %c \t %c \n",c1,c2,c3);
+  printf("then reverse of the characters is\n");
+  printf("%c \t %c \t %c \n",c3,c2,c1);
+}
